01d6d0652b9b5303c34c31931e2ed99799e3f7a3
Aminor-z <1079465529@qq.com>
2020-03-19 11:24:20 +0800

    upload

---
+++ 1st A Let the Balloon Rise/1st A Let the Balloon Rise.cpp
@@ -0,0 +1,43 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	register int n;
+	while (cin >> n)
+	{
+		if (n == 0)break;
+		map<string, int> m;
+		for (register int i = 0; i < n; i++)
+		{
+			string s;
+			cin >> s;
+			m[s]++;
+		}
+		int _max = 0;
+		string out = "";
+		for (map<string, int>::iterator t=m.begin();t!=m.end();t++)
+		{
+			if (_max < t->second)
+			{
+				out = t->first;
+				_max = t->second;
+			}
+		}
+		cout << out << endl;
+	}
+	return 0;
+}
+
---
+++ 1st B Number Sequence/1st B Number Sequence.cpp
@@ -0,0 +1,56 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+int t[10000];
+inline int f(const int& a, const int& b,const int&n)
+{
+	if (n >= 3)
+		return (a * t[n-1] + b * t[n-2])%7;
+	else
+		return 1;
+}
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	register unsigned a, b, n;
+	memset(t, -1, sizeof(t));
+	t[1] = 1;
+	t[2] = 1;
+	
+	while (cin >> a >> b >> n)
+	{
+		if (a == 0 && b == 0 && n == 0)break;
+		for (register int i = 3; i < 300; i++)
+		{
+			t[i] = f(a, b, i);
+		}
+		vector<int> v;
+		v.push_back(t[3]);
+		v.push_back(t[4]);
+		int re = 2;
+		for (register int i = 5; i < 299; i++)
+		{
+			if (t[3] == t[i] && t[4] == t[i + 1]) {
+				break;
+			}
+			else
+			{
+				v.push_back(t[i]);
+				re++;
+			}
+		}
+		cout << v[(n - 3) % re]<< endl;
+	}
+	return 0;
+}
+
